7fb3ea0d7513835d3571d4ad9e4c3f3576a5fe18
ljw <sbr08012@naver.com>
2024-06-16 23:50:53 +0900

    islogin middleware add

--- server.js
+++ server.js
@@ -10,6 +10,9 @@ import env from "dotenv";
 import multer from "multer"; 
 import { Server } from "socket.io";
 import http from "http"; 
+import axios from "axios";
+
+import isLoggedIn from "./auth/isLogged.js"; 
 
 const app = express();
 const port = 3000;
@@ -36,11 +39,11 @@ const server = http.createServer(app);
 const io = new Server(server);
 
 const db = new pg.Client({
-  user: process.env.PG_USER,
-  host: process.env.PG_HOST,
-  database: process.env.PG_DATABASE,
-  password: process.env.PG_PASSWORD,
-  port: process.env.PG_PORT,
+  user: process.env.RDS_USERNAME,
+  host: process.env.RDS_HOSTNAME,
+  database: process.env.RDS_DB_NAME,
+  password: process.env.RDS_PASSWORD,
+  port: process.env.RDS_PORT,
 });
 
 db.connect();
@@ -74,8 +77,7 @@ var uploadpost = multer({
       cb(null, 'public/posts/');
     },
     filename: function (req, file, cb) {
-      const name = file.originalname; 
-      const after = name.split('.'); 
+      const name = Math.random().toString(36).substring(2,11);  
       cb(null, new Date().valueOf() + name);
     }, 
     fileFilter: function (req,file,callback){
@@ -90,13 +92,17 @@ var uploadpost = multer({
     }, 
   })
 });
-app.get("/message", async(req, res)=>{
+app.get("/message", isLoggedIn,async(req, res)=>{
+  
   res.render("message.ejs",{user: req.user, 
     //  user:req.user
       
   });
+
 })
-app.post("/uploadprofile", uploadprofile.single("profile"), async function(req, res){
+app.post("/uploadprofile", isLoggedIn,uploadprofile.single("profile"), async function(req, res){
+  
+  
   try {
     const result = await db.query("update users set profile = $1 where email=$2 ", [
       req.file.filename, req.user.email    
@@ -106,24 +112,27 @@ app.post("/uploadprofile", uploadprofile.single("profile"), async function(req, 
     console.log(err); 
   } 
   res.redirect("/profile"); 
+
 })
 
-app.post("/uploadpost", uploadpost.single("post"), async function(req, res){
+app.post("/uploadpost", isLoggedIn,uploadpost.single("post"), async function(req, res){
+  
   try {  
     const result1 = await db.query("INSERT INTO post (user_email, contents) VALUES ($1, $2) RETURNING *", [
       req.user.email, req.file.filename,     
     ]);
-    console.log(req.file); 
+     
   }catch(err){
     console.log(err); 
   } 
   res.redirect("/profile"); 
+
 })
 
-app.get("/", async(req, res) => {
-  if (req.isAuthenticated()) {
+app.get("/", isLoggedIn,async(req, res) => {
+  
     try {
-
+      
       const result = await db.query("SELECT profile, email, array_length(follower,1) as 팔로워, array_length(followin,1) as 팔로잉  from users order by array_length(follower,1) desc nulls last limit 3 ");
       const userlist = result.rows; 
       const result1 = await db.query("SELECT * FROM users WHERE email = $1 ", [
@@ -139,15 +148,26 @@ app.get("/", async(req, res) => {
   }catch(err){
     console.log(err); 
   }
-  } else {
-    res.redirect("/login");
-  }
+  
 });
 
-app.get("/login", (req, res) => {
-  res.render("login.ejs");
+app.get("/login", async(req, res) => {
+  try{ 
+    const result = await axios.get("https://api.api-ninjas.com/v1/quotes?category=happiness",
+      {headers: {'X-Api-Key': 'g2uItxFEL/dNnJaWdu/GUQ==TQWZTNSig8vKQR0b'}}
+    );
+    
+  res.render("login.ejs", {
+    quote: result.data[0].quote 
+  });
+  }catch(err){
+    console.log(err); 
+    res.status(500); 
+  }
+  
 });
-app.get("/recomment", async(req, res) => {
+app.get("/recomment", isLoggedIn,async(req, res) => {
+  
   const commentid = req.query.commentid;
   const user = req.user; 
   try {  
@@ -162,8 +182,10 @@ app.get("/recomment", async(req, res) => {
   }catch(err){
     console.log(err); 
   }
+
 });
-app.post("/recomment", async(req, res) => {
+app.post("/recomment", isLoggedIn,async(req, res) => {
+  
   const commentid = req.query.commentid;
   const contents = req.body.contents; 
   
@@ -178,9 +200,11 @@ app.post("/recomment", async(req, res) => {
   }catch(err){
     console.log(err); 
   }
+
 });
  
-app.get("/delete", async(req, res)=>{
+app.get("/delete", isLoggedIn,async(req, res)=>{
+  
   const commentid = req.query.commentid;
   const recommentid = req.query.recommentid;
   const postid = req.query.postid;
@@ -205,9 +229,11 @@ app.get("/delete", async(req, res)=>{
   }catch(err){
     console.log(err); 
   }
+
 });
 
-app.get("/like", async(req, res)=>{
+app.get("/like", isLoggedIn,async(req, res)=>{
+  
   const commentid = req.query.commentid;
   const recommentid = req.query.recommentid;
   const postid = req.query.postid;
@@ -232,8 +258,10 @@ app.get("/like", async(req, res)=>{
   }catch(err){
     console.log(err); 
   }
+
 });
-app.get("/private", async(req, res) => {
+app.get("/private", isLoggedIn, async(req, res) => {
+  
   const email = req.query.email; 
   const boolean = req.query.boolean; 
   try {  
@@ -245,8 +273,10 @@ app.get("/private", async(req, res) => {
   catch(err){
     console.log(err); 
   }
+
 }); 
-app.get("/comment", async(req, res) => {
+app.get("/comment", isLoggedIn, async(req, res) => {
+  
   const postid = req.query.postid;
   const postemail = req.query.postemail;
   const user = req.user; 
@@ -263,8 +293,10 @@ app.get("/comment", async(req, res) => {
   }catch(err){
     console.log(err); 
   }
+
 });
-app.post("/comment", async(req, res) => {
+app.post("/comment", isLoggedIn, async(req, res) => {
+  
   const target = req.query.postid;
   const contents = req.body.contents; 
   const postemail = req.query.postemail; 
@@ -279,9 +311,10 @@ app.post("/comment", async(req, res) => {
   }catch(err){
     console.log(err); 
   }
+
 });
-app.get("/profile", async(req, res) => {
-  if (req.isAuthenticated()) {
+app.get("/profile", isLoggedIn, async(req, res) => {
+  
     const useremail=req.user.email; 
     try {
        
@@ -298,28 +331,24 @@ app.get("/profile", async(req, res) => {
     }catch(err){
       console.log(err); 
     } 
-} else {
-  res.redirect("/login");
-}
+
 });
 
-app.get("/search", (req, res) => {
-  if (req.isAuthenticated()) {
+app.get("/search", isLoggedIn,(req, res) => {
+  
   res.render("search.ejs",{
     user: req.user, 
     searcheduser: null 
   });
-} else {
-  res.redirect("/login");
-}
+
 });
 
 app.get("/register", (req, res) => {
   res.render("register.ejs");
 });
 
-app.get("/post", async(req, res) => {
-  if (req.isAuthenticated()) {
+app.get("/post", isLoggedIn,async(req, res) => {
+  
   const target = req.query.email;
   const useremail = req.user.email; 
    
@@ -344,18 +373,16 @@ app.get("/post", async(req, res) => {
   }catch(err){
     console.log(err); 
   }
-} else {
-  res.redirect("/login");
-} 
+
    
 }); 
 
 
 
-app.post("/unfollow", async(req, res) => {
+app.post("/unfollow", isLoggedIn,async(req, res) => {
   const email = req.user.email;
   const target = req.query.email; 
-  if (req.isAuthenticated()){
+  
   try {
     const result = await db.query("update users set followin = array_remove(followin, $1) where email= $2 ", [
       target,email
@@ -371,12 +398,11 @@ app.post("/unfollow", async(req, res) => {
     console.log(err); 
   }
   res.redirect("/"); 
-} else {
-  return res.redirect("/login");
-}
+
 });
 
-app.post("/follow", async(req, res) => {
+app.post("/follow", isLoggedIn,async(req, res) => {
+  
   const email = req.user.email;
   const target = req.query.email;
    
@@ -395,6 +421,7 @@ app.post("/follow", async(req, res) => {
     console.log(err); 
   } 
   return res.redirect("/post?email="+target);
+
 });
 
 app.get("/logout", (req, res) => {
@@ -413,7 +440,8 @@ app.post(
     failureRedirect: "/login",
   })
 );
-app.post("/search", async(req, res) => {
+app.post("/search", isLoggedIn,async(req, res) => {
+  
   const email = req.body.username;
   const user1 = req.user; 
   try {
@@ -437,8 +465,10 @@ app.post("/search", async(req, res) => {
   }catch(err){
     console.log(err);
   }
+
 });
-app.get("/block", async(req, res) => {
+app.get("/block", isLoggedIn,async(req, res) => {
+  
   const email = req.query.email;
   const user = req.user; 
   try {
@@ -450,8 +480,10 @@ app.get("/block", async(req, res) => {
     console.log(err);
   }
   res.redirect("back");
+
 });
-app.get("/unblock", async(req, res) => {
+app.get("/unblock", isLoggedIn,async(req, res) => {
+  
   const email = req.query.email;
   const user = req.user; 
   try {
@@ -463,6 +495,7 @@ app.get("/unblock", async(req, res) => {
     console.log(err);
   }
   res.redirect("back");
+
 });
 app.post("/register", async (req, res) => {
   const email = req.body.username;
@@ -502,7 +535,7 @@ io.on('connection', (socket) => {
   socket.on('join chat', async ({ user1, user2 }) => {
       const room = [user1, user2].sort().join('_');
       socket.join(room);
-      console.log(`User ${user1} and ${user2} joined room: ${room}`);
+      
 
       // 이전 메시지 불러오기
       const result = await db.query('SELECT username, message, timestamp FROM messages WHERE room = $1 ORDER BY timestamp ASC', [room]);
@@ -511,7 +544,7 @@ io.on('connection', (socket) => {
   });
 
   socket.on('disconnect', () => {
-      console.log('user disconnected');
+      
   });
 
   socket.on('chat message', async ({ user1, user2, username, msg }) => {
@@ -559,6 +592,7 @@ app.get(
     scope: ["profile", "email"],
   })
 );
+
 app.get(
   "/auth/google/cb",
   passport.authenticate("google", {
